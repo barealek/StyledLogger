4e7758fa256e2866d8c36a8e76e8383a7e14268e
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-04-20 10:14:50 +0000

    Add setup function

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    # Basics
+    name='styledlogger',
+    version='0.1 PRE',
+    description='A simple, styled logging library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    
+    # Author
+    author='Aleksander',
+    author_email='alek@imalek.me',
+    
+    # Package
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    url='https://github.com/SpLayzDK/StyledLogger/',
+
+    # License
+    license='BSL 1.0',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSL 1.0",
+        "Operating System :: Windows & Linux (Tested)",
+    ],
+
+    # Dependencies
+    python_requires='>=3.7',
+    install_requires=[
+        'colorama',
+    ]
+
+)
