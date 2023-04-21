a6b346728e444e0a427bc7e0857726aef7d44a5a
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-04-21 11:46:54 +0000

    Update setup.py extras require & classifiers

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
     license='BSL 1.0',
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSL 1.0",
-        "Operating System :: Windows & Linux (Tested)",
+        "License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)",
+        "Operating System :: OS Independent"
     ],
 
     # Dependencies
@@ -33,6 +33,13 @@ setup(
     install_requires=[
         'colorama>=0.4.6',
         'arrow>=1.2.3'
-    ]
+    ],
+
+    extras_require={
+        'dev': [
+            'pytest>=7.0',
+            "twine>=4.0.2"
+        ]
+    }
 
 )
