d6494845bcdf515668ab2601ec390b07474e8011
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-04-21 09:44:14 +0000

    Add printing types

---
+++ app/styledlogger/src/classes/printtypes.py
@@ -0,0 +1,18 @@
+class PrintType:
+    def __init__(self):
+        self.display = None
+
+class Debug(PrintType):
+    display = "DEBU"
+
+class Info(PrintType):
+    display = "INFO"
+
+class Warn(PrintType):
+    display = "WARN"
+
+class Error(PrintType):
+    display = "ERRO"
+
+class Fatal(PrintType):
+    display = "FATL"
