236d453ba6514a3a41288db866760081c0468a3d
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-04-21 10:01:56 +0000

    Add styleconfig to root namespace

--- app/styledlogger/__init__.py
+++ app/styledlogger/__init__.py
@@ -1,6 +1,6 @@
 from .src.logger import (
     Logger as StyledLogger
 )
-from .src.classes.printcolors import (
-    Colors as StyledColors
+from .src.classes.styleconfig import (
+    StyleConfig
 )
