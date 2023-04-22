bc5345eb6311b2c3fd4b13de8ea6583a087bceaf
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-04-22 13:06:01 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     # Basics
     name='styledlogger',
-    version='0.0.3',
+    version='0.0.4',
     description='A simple, styled logging library.',
     long_description=long_description,
     long_description_content_type='text/markdown',
