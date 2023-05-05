c53537e0347bf01b45442599ca9bdd163f93f842
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-05-05 15:18:09 +0000

    Add System print

--- styledlogger/logger.py
+++ styledlogger/logger.py
@@ -3,7 +3,7 @@ from colorama import (
 )
 
 from .classes.styleconfig import StyleConfig
-from .classes.printtypes import Debug, Info, Warn, Error, Fatal
+from .classes.printtypes import Debug, Info, Warn, Error, Fatal, System
 
 just_fix_windows_console()
 
@@ -26,13 +26,13 @@ class Logger:
 
     def set_level(self, level):
         """
-        Set the log level
+        Set the log level. 0 = debug, 1 = info, 2 = warn, 3 = error, 4 = fatal. All prints lower than the level will be ignored.
         """
         self.level = level
 
     def debug(self, message):
         """
-        Log an info message
+        Log a debug message
         """
         if self.level <= 0:
             self._log(self.style_config.style_text(self.name, Debug, message))
@@ -46,24 +46,31 @@ class Logger:
 
     def warn(self, message):
         """
-        Log an info message
+        Log a warning message
         """
         if self.level <= 2:
             self._log(self.style_config.style_text(self.name, Warn, message))
 
     def error(self, message):
         """
-        Log an info message
+        Log an error message
         """
         if self.level <= 3:
             self._log(self.style_config.style_text(self.name, Error, message))
     
     def fatal(self, message):
         """
-        Log an info message
+        Log a fatal message
         """
         if self.level <= 4:
             self._log(self.style_config.style_text(self.name, Fatal, message))
+
+    def system(self, message):
+        """
+        Log a system message
+        """
+        if self.level <= 5:
+            self._log(self.style_config.style_text(self.name, System, message))
 
     def _log(self, message):
         if self.is_muted:
@@ -78,12 +85,12 @@ class Logger:
 
     def mute(self):
         """
-        Mute the logger
+        Mute the logger.
         """
         self.is_muted = True
 
     def unmute(self):
         """
-        Unmute the logger
+        Unmute the logger.
         """
         self.is_muted = False
