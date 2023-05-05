80836c28f8bac601dfe03160e8969c06bf7d4675
Alek <92643775+SpLayzDK@users.noreply.github.com>
2023-05-05 15:19:15 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     # Basics
     name='styledlogger',
-    version='0.0.4',
+    version='0.0.5',
     description='A simple, styled logging library.',
     long_description=long_description,
     long_description_content_type='text/markdown',
