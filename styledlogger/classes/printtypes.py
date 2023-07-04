718d166457634987609915dde6c9af8e288e42c8
Alek <92643775+barealek@users.noreply.github.com>
2023-07-04 08:32:17 +0000

    Add levels to each print type

--- styledlogger/classes/printtypes.py
+++ styledlogger/classes/printtypes.py
@@ -11,23 +11,29 @@ class PrintType:
 
 class Debug(PrintType):
     display = "DEBU"
+    level = 0
 
 
 class Info(PrintType):
     display = "INFO"
+    level = 1
 
 
 class Warn(PrintType):
     display = "WARN"
+    level = 2
 
 
 class Error(PrintType):
     display = "ERRO"
+    level = 3
 
 
 class Fatal(PrintType):
     display = "FATL"
+    level = 4
 
 
 class System(PrintType):
     display = "SYST"
+    level = 5
