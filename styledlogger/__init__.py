458234b856909db9010d7fcfc3b7b12ec38ee0eb
Alek <92643775+barealek@users.noreply.github.com>
2023-07-04 08:31:12 +0000

    Add callback to imports and format with black

--- styledlogger/__init__.py
+++ styledlogger/__init__.py
@@ -1,10 +1,8 @@
-from .logger import (
-    Logger as StyledLogger
-)
+from .logger import Logger as StyledLogger
 
-from .classes.styleconfig import (
-    StyleConfig
-)
+from .classes.styleconfig import StyleConfig
+
+from .classes.callback import Callback
 
 __AUTHOR__ = "ImAlek (https://github.com/barealek)"
 __LICENSE__ = "See https://github.com/barealek/StyledLogger for license"
