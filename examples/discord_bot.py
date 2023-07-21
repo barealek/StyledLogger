3919d0d33dfbd4adf20133f485e8bed6e857cac3
Alek <92643775+barealek@users.noreply.github.com>
2023-07-21 17:30:22 +0200

    Add example with a Discord bot

---
+++ examples/discord_bot.py
@@ -0,0 +1,18 @@
+from styledlogger import StyledLogger
+import discord
+
+bot = discord.Client()
+bot.logger = StyledLogger("discord")
+
+@bot.event
+async def on_ready():
+    bot.logger.info("Logged in!")
+
+@bot.event
+async def on_message(message):
+    bot.logger.debug("Message received!")
+    if message.content == "ping":
+        await message.channel.send("pong")
+
+bot.logger.info("Starting bot...")
+bot.run(...)
