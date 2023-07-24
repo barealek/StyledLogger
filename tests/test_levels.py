b3c4e9f7883d25a4e619ff4f4f12801f3263c690
Alek <92643775+barealek@users.noreply.github.com>
2023-07-24 14:32:30 +0200

    Add a test for the levels

---
+++ tests/test_levels.py
@@ -0,0 +1,50 @@
+
+import unittest
+from styledlogger import StyledLogger as Logger, CallbackContext
+
+
+class TestLevels(unittest.TestCase):
+
+    def test_levels(self):
+        logger = Logger("test_levels", level=0)
+        self.assertEqual(logger.level, 0)
+        self.assertEqual(logger.debug("Debug message"), True)
+        self.assertEqual(logger.info("Info message"), True)
+        self.assertEqual(logger.warn("Warn message"), True)
+        self.assertEqual(logger.error("Error message"), True)
+        self.assertEqual(logger.fatal("Fatal message"), True)
+        logger.set_level(1)
+        self.assertEqual(logger.level, 1)
+        self.assertEqual(logger.debug("Debug message"), False)
+        self.assertEqual(logger.info("Info message"), True)
+        self.assertEqual(logger.warn("Warn message"), True)
+        self.assertEqual(logger.error("Error message"), True)
+        self.assertEqual(logger.fatal("Fatal message"), True)
+        logger.set_level(2)
+        self.assertEqual(logger.level, 2)
+        self.assertEqual(logger.debug("Debug message"), False)
+        self.assertEqual(logger.info("Info message"), False)
+        self.assertEqual(logger.warn("Warn message"), True)
+        self.assertEqual(logger.error("Error message"), True)
+        self.assertEqual(logger.fatal("Fatal message"), True)
+        logger.set_level(3)
+        self.assertEqual(logger.level, 3)
+        self.assertEqual(logger.debug("Debug message"), False)
+        self.assertEqual(logger.info("Info message"), False)
+        self.assertEqual(logger.warn("Warn message"), False)
+        self.assertEqual(logger.error("Error message"), True)
+        self.assertEqual(logger.fatal("Fatal message"), True)
+        logger.set_level(4)
+        self.assertEqual(logger.level, 4)
+        self.assertEqual(logger.debug("Debug message"), False)
+        self.assertEqual(logger.info("Info message"), False)
+        self.assertEqual(logger.warn("Warn message"), False)
+        self.assertEqual(logger.error("Error message"), False)
+        self.assertEqual(logger.fatal("Fatal message"), True)
+        
+        self.assertEqual(logger.system("System message"), True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
