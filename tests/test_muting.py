4c92b5015eda744040a5876207b4d9d1e369100b
Alek <92643775+barealek@users.noreply.github.com>
2023-07-24 14:32:36 +0200

    Add a test for muting

---
+++ tests/test_muting.py
@@ -0,0 +1,29 @@
+
+import unittest
+from styledlogger import StyledLogger as Logger, CallbackContext
+
+
+class TestMuting(unittest.TestCase):
+
+    def test_muting(self):
+        logger = Logger("test_muting", level=4)
+        logger.mute()
+        self.assertTrue(logger.is_muted)
+        self.assertFalse(logger.debug("Debug message"))
+        self.assertFalse(logger.info("Info message"))
+        self.assertFalse(logger.warn("Warn message"))
+        self.assertFalse(logger.error("Error message"))
+        self.assertFalse(logger.fatal("Fatal message"))
+        self.assertFalse(logger.system("System message"))
+        logger.unmute()
+        self.assertFalse(logger.is_muted)
+        self.assertFalse(logger.debug("Debug message"))
+        self.assertFalse(logger.info("Info message"))
+        self.assertFalse(logger.warn("Warn message"))
+        self.assertFalse(logger.error("Error message"))
+        self.assertTrue(logger.fatal("Fatal message"))
+        self.assertTrue(logger.system("System message"))
+
+
+if __name__ == '__main__':
+    unittest.main()
