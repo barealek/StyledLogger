37a50c1bad5702c76a4287deddc5b613ffa07ffc
Alek <92643775+barealek@users.noreply.github.com>
2023-07-24 15:07:05 +0200

    Add a test for unstyled formatting

--- tests/test_style_configs.py
+++ tests/test_style_configs.py
@@ -1,6 +1,8 @@
 import unittest
 from styledlogger.classes.styleconfig import StyleConfig
 from styledlogger.classes.printcolors import Colors
+from styledlogger.classes.printtypes import Warn
+from arrow import now
 
 
 class TestStyleConfig(unittest.TestCase):
@@ -27,6 +29,24 @@ class TestStyleConfig(unittest.TestCase):
         self.assertEqual(style_config.warn_color, Colors.YELLOW)
         self.assertEqual(style_config.error_color, Colors.RED)
 
+        self.assertEqual(style_config.reset, Colors.RESET)
+
+    def test_style_text_uncolored(self):
+        style_config = StyleConfig(
+            text_format="{time} {name} {text}",
+            time_format="YYYY-MM-DD HH:mm",
+            time_color="blue",
+            name_color="green",
+            text_color="light_gray",
+            debug_color="cyan",
+            info_color="green",
+            warn_color="yellow",
+            error_color="red"
+        )
+
+        text = style_config.style_text_uncolored("logger", Warn, "Test message")
+        rn = now().format("YYYY-MM-DD HH:mm")
+        expected_text = f"{rn} logger Test message"
 
 if __name__ == '__main__':
     unittest.main()
