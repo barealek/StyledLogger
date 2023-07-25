936505b4f95430b05b1f4595ca6dd2a079e9d432
Alek <92643775+barealek@users.noreply.github.com>
2023-07-25 17:08:38 +0200

    Make tests script return error count

--- tests/__main__.py
+++ tests/__main__.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     loader = unittest.TestLoader()
     suite = loader.discover("tests", pattern="test_*.py")
     runner = unittest.TextTestRunner()
-    runner.run(suite)
+    results = runner.run(suite)
+    exit(len(results.failures) + len(results.errors))
